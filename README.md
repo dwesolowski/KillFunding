6cd6194ed48f6e98d88af60a78f43a87541b6858
Daren Wesolowski <daren.wesolowski@icloud.com>
2019-03-12 14:25:19 -0400

    Initial import

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-KillFunding
+# KillFunding - Plugin for Bukkit | Spigot
+
+[![Software License](http://img.shields.io/badge/License-MIT-brightgreen.svg?style=flat-square)](LICENSE)
+
+Credit players money when they kill others!
+
+#### Supported Versions: 1.13+
+
+- Give players money for killing other players
+- Make players lose money when killed by another player
+- Configurable config.yml file
+- Permissions and Group support
+
+## Installation
+
+To use this plugin place the .jar and any other files in your plugins directory and restart the server.
---
+++ src/main/java/com/github/dwesolowski/killfunding/KillFunding.java
@@ -0,0 +1,154 @@
+package com.github.dwesolowski.killfunding;
+
+import org.bukkit.plugin.java.*;
+import org.bukkit.plugin.*;
+import org.bukkit.event.entity.*;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import net.milkbowl.vault.economy.*;
+import org.bukkit.event.*;
+import org.bukkit.command.*;
+
+public class KillFunding extends JavaPlugin implements Listener {
+    private static Economy economy;
+
+    static {
+        KillFunding.economy = null;
+    }
+
+    @Override
+    public void onEnable() {
+        registerConfig();
+        registerEvents();
+        registerEconomy();
+        registerMetrics();
+    }
+
+    private void registerConfig() {
+        saveDefaultConfig();
+        saveConfig();
+    }
+
+    private void registerEvents() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    private void registerEconomy() {
+        if (!this.setupEconomy()) {
+            this.getLogger().severe("Plugin was disabled because an economy system was not found!");
+            this.getServer().getPluginManager().disablePlugin(this);
+        }
+    }
+
+    private void registerMetrics() {
+        final MetricsLite metrics = new MetricsLite(this);
+    }
+
+    private boolean setupEconomy() {
+        final RegisteredServiceProvider<Economy> economyProvider = this.getServer().getServicesManager().getRegistration(Economy.class);
+        if (economyProvider != null) {
+            KillFunding.economy = economyProvider.getProvider();
+        }
+        return KillFunding.economy != null;
+    }
+
+    public static boolean isInt(final String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    @EventHandler
+    public void playerDeath(final EntityDeathEvent e) {
+        final String prefix = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.prefix"));
+        final Entity victim = e.getEntity();
+        if (victim instanceof Player) {
+            final Entity killer = e.getEntity().getKiller();
+            if (killer != null) {
+                boolean kHasPermission = false;
+                boolean vHasPermission = false;
+                double accountCredits = 0;
+                for (final String s : this.getConfig().getConfigurationSection("groups").getKeys(false)) {
+                    if (victim.hasPermission(this.getConfig().getString("groups." + s + ".permission"))) {
+                        accountCredits = this.getConfig().getInt("groups." + s + ".amount");
+                        vHasPermission = true;
+                        break;
+                    }
+                }
+                for (final String s : this.getConfig().getConfigurationSection("groups").getKeys(false)) {
+                    if (killer.hasPermission(this.getConfig().getString("groups." + s + ".permission"))) {
+                        accountCredits = this.getConfig().getInt("groups." + s + ".amount");
+                        kHasPermission = true;
+                        break;
+                    }
+                }
+                double vBalance = KillFunding.economy.getBalance((OfflinePlayer) victim);
+                if (vBalance > 0) {
+                    boolean vHasBalance = true;
+                    if (vBalance <= accountCredits) {
+                        accountCredits = vBalance;
+                        vHasBalance = false;
+                    }
+                    if (vHasPermission && !victim.getName().equals(killer.getName())) {
+                        final EconomyResponse takeMoney = KillFunding.economy.withdrawPlayer((OfflinePlayer) victim, accountCredits);
+                        if (takeMoney.transactionSuccess() && accountCredits != 0) {
+                            final String money = Double.toString(accountCredits);
+                            if (this.getConfig().getString("options.enable-prefix").equals("true")) {
+                                victim.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + " " + this.getConfig().getString("messages.death-message").replace("{killer}", victim.getName()).replace("{killer}", killer.getName()).replace("{lost-money}", money)));
+                            } else {
+                                victim.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.death-message").replace("{killer}", victim.getName()).replace("{killer}", killer.getName()).replace("{lost-money}", money)));
+                            }
+                        }
+                    }
+                    if (kHasPermission && !killer.getName().equals(victim.getName())) {
+                        final EconomyResponse giveMoney = KillFunding.economy.depositPlayer((OfflinePlayer) killer, accountCredits);
+                        if (giveMoney.transactionSuccess()) {
+                            final String money = Double.toString(accountCredits);
+                            if (vHasBalance) {
+                                if (this.getConfig().getString("options.enable-prefix").equals("true")) {
+                                    killer.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + " " + this.getConfig().getString("messages.kill-message").replace("{victim}", victim.getName()).replace("{killer}", killer.getName()).replace("{reward-money}", money)));
+                                } else {
+                                    killer.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.kill-message").replace("{victim}", victim.getName()).replace("{killer}", killer.getName()).replace("{reward-money}", money)));
+                                }
+                            } else {
+                                if (this.getConfig().getString("options.enable-prefix").equals("true")) {
+                                    killer.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + " " + this.getConfig().getString("messages.kill-low-balance-message").replace("{victim}", victim.getName()).replace("{killer}", killer.getName()).replace("{reward-money}", money)));
+                                } else {
+                                    killer.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("kill-low-balance-message").replace("{victim}", victim.getName()).replace("{killer}", killer.getName()).replace("{reward-money}", money)));
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    if (this.getConfig().getString("options.enable-prefix").equals("true")) {
+                        killer.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + " " + this.getConfig().getString("messages.kill-no-balance-message").replace("{victim}", victim.getName())));
+                    } else {
+                        killer.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("kill-no-balance-message").replace("{victim}", victim.getName())));
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean onCommand(final CommandSender sender, final Command cmd, final String commandLabel, final String[] args) {
+        final String noPermission = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.no-permission"));
+        if (commandLabel.equalsIgnoreCase("kfreload")) {
+            if (sender instanceof Player) {
+                final Player player = (Player) sender;
+                if (player.hasPermission("killfund.reload")) {
+                    sender.sendMessage(ChatColor.GREEN + "The plugin has been reloaded!");
+                    this.reloadConfig();
+                } else if (!player.hasPermission("killfund.reload")) {
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', noPermission));
+                }
+            } else {
+                sender.sendMessage(ChatColor.GREEN + "The plugin has been reloaded!");
+                this.reloadConfig();
+            }
+        }
+        return false;
+    }
+}
